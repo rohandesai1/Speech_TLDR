6e40e2e413b004139d8c5381c061429079308eae
rohandesai1 <126644574+rohandesai1@users.noreply.github.com>
2023-07-10 19:48:01 -0700

    Get transcript from YT link

---
+++ transcript.py
@@ -0,0 +1,33 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+class Video:
+    def __init__(self, link):
+        self.link = link
+        self.ID = link[link.index("=") + 1:]
+    
+    def getTranscriptText(self):
+        transcript = YouTubeTranscriptApi.get_transcript(self.ID)
+        transcriptText = ""
+        for entry in transcript:
+            transcriptText += " " + entry["text"]
+        
+        return transcript
+
+link = input("Link: ")
+vid = Video(link)
+print(vid.getTranscriptText())
+"""
+
+url = "https://gpt-summarization.p.rapidapi.com/summarize"
+
+info = {"text" : transcriptText, "num_sentences" : 20}
+
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": "707002898dmshcc9f17c8e05c13dp1c3471jsn4a5c26b80ae6",
+	"X-RapidAPI-Host": "gpt-summarization.p.rapidapi.com"
+}
+
+response = requests.post(url, json=info, headers=headers)
+
+print(response.json())"""
