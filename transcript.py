7bf9f4be05c487c2e75d1c0b16cfbd5c0fce3c58
rohandesai1 <126644574+rohandesai1@users.noreply.github.com>
2023-08-23 22:33:28 -0700

    add __name__ == "__main__

--- transcript.py
+++ transcript.py
@@ -1,11 +1,11 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
 class Video:
-    def __init__(self, link):
+    def __init__(self, link : str):
         self.link = link
         self.ID = link[link.index("=") + 1:]
     
-    def getTranscriptText(self):
+    def getTranscriptText(self) -> str:
         transcript = YouTubeTranscriptApi.get_transcript(self.ID)
         transcriptText = ""
         for entry in transcript:
@@ -13,22 +13,9 @@ class Video:
         
         return transcript
 
-link = input("Link: ")
-vid = Video(link)
-print(vid.getTranscriptText())
 
-"""
+if __name__ == "__main__":
+    link = input("Link: ")
+    vid = Video(link)
+    print(vid.getTranscriptText())
 
-url = "https://gpt-summarization.p.rapidapi.com/summarize"
-
-info = {"text" : transcriptText, "num_sentences" : 20}
-
-headers = {
-	"content-type": "application/json",
-	"X-RapidAPI-Key": "707002898dmshcc9f17c8e05c13dp1c3471jsn4a5c26b80ae6",
-	"X-RapidAPI-Host": "gpt-summarization.p.rapidapi.com"
-}
-
-response = requests.post(url, json=info, headers=headers)
-
-print(response.json())"""
