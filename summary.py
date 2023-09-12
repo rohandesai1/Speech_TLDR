682244f2236d5d0aead2a19f9bfc2d606d20b666
rohandesai1 <126644574+rohandesai1@users.noreply.github.com>
2023-09-12 16:53:36 -0700

    allow summarization of any-length video

--- summary.py
+++ summary.py
@@ -10,7 +10,9 @@ class Summary:
         path = Path(".env")
         load_dotenv(dotenv_path = path)
         openai.api_key = os.environ["openai_key"]
-        self.transcript = transcript
+        self.full_transcript = transcript
+        self.text_transcript = " ".join([val["text"] for val in self.full_transcript])
+        
     def tokenCount(self,text):
         print(0.3679 * len(text))
         words = text
@@ -22,22 +24,50 @@ class Summary:
         tokensPerWord = avgWordLength/tokenLength
         return tokensPerWord * len(words)
     
+
     def getSummary(self):
-        text = f"marked by three dashes, there will be a transcript from a youtube video. your job is to provide a 150 word summary of the video. ---{self.transcript}---"
-        print(self.tokenCount(text))
-        message=[{"role": "user", "content": text}]
-        try:
-            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k-0613", messages = message, temperature = 0.0)
-            print(completion.choices[0].message.content)
-        except openai.error.InvalidRequestError:
-            print("SORRY VIDEO TOO LONG. ADDING SUMMARY OF LONG-VIDEOS SOON. ")
-        
+            def split(parts):
+                try:
+                    
+                    texts = []
+                    
+                    for i,part in enumerate(parts):
+                        print(f"part {i+1}")
+                        if len(parts) == 1:
+                            message=[{"role": "user", "content": f"marked by three dashes, there will be a transcript from a youtube video. your job is to provide a 150 word summary of the video. ---{part}---"}]
+                        elif i > 0 and i < len(parts) - 1:
+                            past_summary = " ".join([summ for summ in texts])
+                            message=[{"role": "user", "content": f"marked by three dashes, there will be an excerpt of a transcript from a youtube video. marked with three backslashes there will be the summary so far. please continue the summary of this video based on the summary so far and the transcript. ---{part}--- |||{past_summary}|||"}]
+                        elif i == 0:
+                            message=[{"role": "user", "content": f"marked by three dashes, there will be an excerpt of a transcript from a youtube video. this excerpt is the start of the video. please start the summary of this youtube video based on the transcript. ---{part}---"}]
+                        else:
+                            past_summary = " ".join([summ for summ in texts])
+                            message=[{"role": "user", "content": f"marked by three dashes, there will be an excerpt of a transcript from a youtube video. marked with three backslashes there will be the summary so far. this transcript is the final part of the youtube video, please finish the summary. ---{part}--- |||{past_summary}|||"}]
+
+                            
+                        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k-0613", messages = message, temperature = 0.0)
+                        texts.append(completion.choices[0].message.content)
+
+                    self.final = texts
+                    return
+                except openai.error.InvalidRequestError:
+                    print("split")
+                    new_parts = []
+                    for part in parts:
+                        new_parts.append(part[0:int(len(part)/2)])    
+                        new_parts.append(part[int(len(part)/2):len(part) - 1])
+                    split(new_parts)
+
+            split([self.text_transcript])
+
+            return " ".join(self.final)
+
 
 def main():
     vid = Video(input("Enter youtube video link: "))
     transcript = vid.getTranscriptText()
     summary = Summary(transcript)
-    summary.getSummary()
+    print(summary.getSummary())
     
 if __name__ == "__main__":
     main()
